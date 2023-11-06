7d8f753f4a4689efdbedc39a71aabe050e7ce006
Vincentime <vincentime147@gmail.com>
2023-11-06 17:30:35 +0100

    sav tmp

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class AcceuilController extends AbstractController
@@ -23,4 +25,18 @@ class AcceuilController extends AbstractController
             'controller_name' => 'AcceuilController',
         ]);
     }
+
+    #[Route('/showProducts')] 
+    public function showProducts(Request $request) { 
+        $product = $request->query->get('product', 'defaultProduct');
+                // Affichage du résultat 
+        // Vérification de la présence de 'category' 
+        $hasCategory = $request->query->has('category'); 
+        
+        // Récupération des clés des paramètres 
+        $paramKeys = $request->query->keys(); 
+        // Affichage des résultats 
+        dump($product, $hasCategory, $paramKeys);
+        
+        return new Response('<h1>Utilisation de l\'objet Request dans un Contrôleur<h1>'); }
 }
